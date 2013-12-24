b2e704769e6f0b0d8e9eb440e20d357d625c6ce6
Izhar Gavish <izhar@tikalk.com>
2013-12-24 11:37:37 +0200

    form action created

--- server/server.rb
+++ server/server.rb
@@ -1,6 +1,13 @@
 require "bundler/setup"
 require "sinatra"
+require "haml"
 
 get "/" do
   "Hello world!"
+end
+
+post "/form" do
+  url = params[:url]
+  id = url.match(/.+\?v=(.+)$/){ $1 }
+  haml: :result
 end
