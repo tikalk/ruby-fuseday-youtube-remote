30633021a3fd49ba14b4478e5a93969e94ef72e1
ereztikal <erez@tikalk.com>
2013-12-24 15:13:06 +0200

    Added websocket

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,9 +4,9 @@ myApp.controller("PlayerCtrl" ,function ($scope, wsService) {
     $scope.video = '';
 
     wsService.subscribe(function(message) {
-        $scope.messages = message.youtube_video_id;
+        $scope.video = message.youtube_video_id;
         $scope.$apply();
     });
 
-    wsService.connect();
+    //wsService.connect();
 });
