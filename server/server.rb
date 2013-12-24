ec2cc2a8cf402decaa9aef435307a5b5058b2ea8
Izhar Gavish <izhar@tikalk.com>
2013-12-24 12:00:23 +0200

    added params to log

--- server/server.rb
+++ server/server.rb
@@ -3,12 +3,17 @@ require 'bundler'
 Bundler.require
 require 'sinatra'
 
+before do
+  puts '[Params]'
+  p params
+end
+
 get "/" do
   haml :controller
 end
 
 post "/form" do
   url = params[:url]
-  id = url.match(/.+\?v=(.+)$/){ $1 }
+  @id = url.match(/.+\?v=(.+)$/){ $1 }
   haml :result
 end
