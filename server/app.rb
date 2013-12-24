b4019ea360b1b29887af19bcaa39db6bffed3cad
Meital Federgreen <zask.mz@gmail.com>
2013-12-24 15:27:24 +0200

    Recently viewed uses Redis

--- server/app.rb
+++ server/app.rb
@@ -1,6 +1,7 @@
 require 'em-websocket'
 require 'em-hiredis'
-
+require 'json/ext'
+require 'sinatra/flash'
 
 before do
   puts '[Params]'
@@ -8,6 +9,8 @@ before do
 end
 
 get "/" do
+  redis = Redis.new
+  @viewed_items = redis.smembers "recently-viewed"
   haml :controller
 end
 
@@ -18,5 +21,12 @@ end
 post "/form" do
   url = params[:url]
   @id = url.match(/.+\?v=(.+)$/){ $1 }
-  haml :result
+
+  video = VideoInfo.new("http://www.youtube.com/watch?v=#{@id}")
+  video_hash = {id: video.video_id, title: video.title, description: video.description, thumbnail: video.thumbnail_medium}.to_json
+  redis = Redis.new
+  redis.sadd "recently-viewed", video_hash
+
+  flash[:result] = @id
+  redirect "/"  
 end
