41975bfb396253ea5e0e952c701d8bbb3ed6752c
KensoDev <avi@kensodev.com>
2013-12-24 13:15:30 +0200

    Basebox configuration

--- devops/roles/basebox.rb
+++ devops/roles/basebox.rb
@@ -1,7 +1,9 @@
 name 'basebox'
 
 run_list [
+  'recipe[mongodb::10gen_repo]',
   'recipe[tikal-redis]',
   'recipe[tikal-memcache]',
-  'recipe[tikal-solr]'
+  'recipe[tikal-solr]',
+  'recipe[tikal-mongo]'
 ]
